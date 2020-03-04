75be947710b5d81c2ae66ba8748975a8150a243d
shourovys <shourovys@gmail.com>
2020-03-04 13:19:37 +0600

    only + btn work

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-document.getElementById("iphonePlus").addEventListener("click", function(){
-
+document.getElementById("iphonePlus").addEventListener("click", function() {
     const IphoneBuyNum = document.getElementById("iphoneBuyNum").innerText;
-    const iphoneNumber= parseFloat(IphoneBuyNum);
+    const iphoneNumber = parseFloat(IphoneBuyNum);
 
-    document.getElementById("iphoneBuyNum").innerText=9;
+    document.getElementById("iphoneBuyNum").value++;
 
-})
+    const IphonePrice = document.getElementById("iphonePrice").innerText;
+    const iphonePriceNum = parseFloat(IphonePrice);
 
-document.getElementById("iphonePlus").addEventListener("click", function(){
-    const IphoneBuyNum = document.getElementById("iphoneBuyNum").innerText;
-    const iphoneNumber= parseFloat(IphoneBuyNum);
-
-    document.getElementById("iphoneBuyNum").innerText=iphoneNumber+1;
-  });
+    document.getElementById("iphonePrice").innerText = iphonePriceNum + 1219;
 
 
+    const TotalCheckouta = document.getElementById("TotalCheckout").innerText;
+    const TotalCheckoutNum = parseFloat(TotalCheckouta);
+
+    document.getElementById("TotalCheckout").innerText = TotalCheckoutNum + 1219;
+});
